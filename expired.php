68f8113c2cbf7d8499c18018f65040548acde211
dodypribadi5 <dodypribadi5@gmail.com>
2025-08-13 14:43:27 +0700

    Delete expired.php

--- expired.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Jakarta');
-$waktuBatas = strtotime('2025-08-22 23:59:59');
-
-if (time() > $waktuBatas) {
-  header('Location: https://expiredweb.pages.dev');
-  exit;
-}
-?>
